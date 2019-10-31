e1ad35b48587b37db08115e1f6fe35f4d3ea1a79
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-10-31 23:38:05 +0200

    update travis

--- config.sh
+++ config.sh
@@ -4,6 +4,18 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
+
+    # Install anaconda
+    yum install curl
+    yum install bzip2
+    yum install cmake
+
+    curl –O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+    export PATH=" $HOME/miniconda3/bin:$PATH"
+    conda -V
+
+
     yum install swig -y
     rm -rf build
     mkdir build
