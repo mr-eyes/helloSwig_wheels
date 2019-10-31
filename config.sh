9b52df0a28c5f457d4edb927865448934c8079da
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-10-31 23:45:30 +0200

    update travis

--- config.sh
+++ config.sh
@@ -12,7 +12,7 @@ function pre_build {
     yum install make -y
 
     curl –O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+    sh Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
     export PATH=" $HOME/miniconda3/bin:$PATH"
     conda -V
 
