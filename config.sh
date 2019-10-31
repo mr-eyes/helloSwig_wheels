298e5785c99638417b8d02356b2957018a5419c4
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-10-31 23:18:39 +0200

    update travis

---
+++ config.sh
@@ -0,0 +1,35 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    yum install swig -y
+    rm -rf build
+    mkdir build
+    cd build
+    cmake ..
+    make
+    cd ..
+}
+
+function pip_opts {
+    # Extra options for pip
+    echo "--only-binary matplotlib --find-links ${MANYLINUX_URL}"
+}
+
+function run_tests {
+
+    # Runs tests on installed distribution from an empty directory
+    ASTROPY_INSTALL_DIR=$(dirname $(python -c 'import astropy; print(astropy.__file__)'))
+
+    # Patch the test_roundtrip_sky_rotation test which is affected by angle
+    # wrapping on 32-bit platforms. This should be fixed in astropy v4.0 with
+    # https://github.com/astropy/astropy/pull/9496
+    local patch_file=$(abspath ../patches/test_roundtrip_sky_rotation.patch)
+    (cd $ASTROPY_INSTALL_DIR && patch -p0 < $patch_file)
+
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    python -c "import sys; import astropy; sys.exit(astropy.test(remote_data='none'))"
+}
