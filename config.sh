dc434f07f3ce84f4ad5a4565ae8d776e1f78a5f9
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-10-31 23:42:01 +0200

    update travis

--- config.sh
+++ config.sh
@@ -6,9 +6,10 @@ function pre_build {
     # Runs in the root directory of this repository.
 
     # Install anaconda
-    yum install curl
-    yum install bzip2
-    yum install cmake
+    yum install curl -y
+    yum install bzip2 -y
+    yum install cmake -y
+    yum install make -y
 
     curl –O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
